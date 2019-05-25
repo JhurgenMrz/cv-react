a55ec4e68b409e2cda7b5a4bde6abe1b1ebb690b
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2019-05-25 13:21:45 -0300

    Styled Component Agregado

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 const Certificates = props =>(
     <div className="Certificates">
         <div className="Certificates-container">
+        <h2>Certificates</h2>
 
-            {props.data.map((cert,index)=>{
+            {props.data.map((cert,index)=>(
             <div className="Certificates-item" key={`Cert-${index}`}>
-                <h3>{`${cert.name} - ${cert.institution} ${cert.date}`}</h3>
+                <h3>{cert.name} @{cert.institution} {cert.date}</h3>
                 <p>{cert.description}</p>
             </div>
-            })
+            ))
             }
 
         </div>
