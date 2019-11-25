b0a6e732b8ec5685dfb5c11598ee8db1f5d55abe
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2019-11-25 20:48:25 -0300

    Plss ahora sii

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -17,13 +17,13 @@ function Projects({ projects }) {
   return (
     <div className="Projects-container">
       {projects.map((project, index) => (
-        <div className="Project-item">
+        <div className="Project-item" key={`Project-${index}`}>
           <div className="Project-description">
             <h3>{project.name}</h3>
             <p>{project.description}</p>
             <div className="Project-technologies">
               {project.technologies.map((tech, index) => (
-                <div className="Project-tech">
+                <div className="Project-tech" key={`tech-${index}`}>
                   <p>{tech.name}</p> 
                   {
                     tech.icon_url ? <img src={tech.icon_url} alt={tech.name} /> : DisplayIcon(tech.icon_name)
